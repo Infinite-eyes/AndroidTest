9dbaa7dc3804d4b07964752e3e3ae8f1cdd1d88b
Infinite-eyes <cwm930215@gmail.com>
2020-07-11 18:11:29 +0800

    1.add MotionLayout

--- app/src/main/java/com/demo/lottie/LottieActivity.kt
+++ app/src/main/java/com/demo/lottie/LottieActivity.kt
@@ -10,6 +10,7 @@ import android.view.View
 import android.widget.Button
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.motion.widget.MotionLayout
 import com.airbnb.lottie.LottieAnimationView
 import com.demo.androidtest.R
 
@@ -28,6 +29,7 @@ class LottieActivity : AppCompatActivity() {
 
         val lav = findViewById<LottieAnimationView>(R.id.lav)
         val img = findViewById<ImageView>(R.id.img)
+        val motionLayout = findViewById<MotionLayout>(R.id.motion_layout)
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
 //            img.setOnClickListener {
 //                (img.drawable as AnimatedVectorDrawable).start()
@@ -36,6 +38,27 @@ class LottieActivity : AppCompatActivity() {
                 lav.playAnimation()
             }
         }
+
+        motionLayout.setTransitionListener(object : MotionLayout.TransitionListener {
+            override fun onTransitionTrigger(p0: MotionLayout?, p1: Int, p2: Boolean, p3: Float) {
+//                TODO("Not yet implemented")
+            }
+
+            override fun onTransitionStarted(p0: MotionLayout?, p1: Int, p2: Int) {
+//                TODO("Not yet implemented")
+            }
+
+            override fun onTransitionChange(p0: MotionLayout?, p1: Int, p2: Int, p3: Float) {
+//                TODO("Not yet implemented")
+                lav.progress = p3
+            }
+
+            override fun onTransitionCompleted(p0: MotionLayout?, p1: Int) {
+//                TODO("Not yet implemented")
+            }
+
+        })
+
 
     }
 
