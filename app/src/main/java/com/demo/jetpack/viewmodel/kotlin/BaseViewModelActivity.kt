59cfc210bc111b6d95a54cacbd3d5b4a16823011
Infinite-eyes <cwm930215@gmail.com>
2020-07-12 22:07:17 +0800

    1.add BaseViewModelActivity

---
+++ app/src/main/java/com/demo/jetpack/viewmodel/kotlin/BaseViewModelActivity.kt
@@ -0,0 +1,42 @@
+package com.demo.jetpack.viewmodel.kotlin
+
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import androidx.fragment.app.FragmentActivity
+import androidx.lifecycle.ViewModelProviders
+import com.demo.androidtest.R
+import kotlinx.android.synthetic.main.jepack_base_viewmodel_activity.*
+
+/**
+ * @author chenweiming
+ * @version 1.0
+ * @mail cwm930215@gmail.com
+ * @since 2020/7/12 9:53 PM
+ **/
+class BaseViewModelActivity : FragmentActivity() {
+
+    private var mContentTV: TextView? = null
+    public override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.jepack_base_viewmodel_activity)
+
+        val myViewModel = ViewModelProviders.of(this).get(MyViewModel::class.java)
+
+        //旋转屏幕数据不丢失
+        tv_content.text = myViewModel.number.toString();
+
+
+        btn1.setOnClickListener {
+            myViewModel.number += 1
+            tv_content.text = myViewModel.number.toString()
+
+        }
+        btn2.setOnClickListener {
+            myViewModel.number += 2
+            tv_content.text = myViewModel.number.toString()
+        }
+
+
+    }
+}
---
+++ app/src/main/java/com/demo/jetpack/viewmodel/kotlin/MyViewModel.kt
@@ -0,0 +1,13 @@
+package com.demo.jetpack.viewmodel.kotlin
+
+import androidx.lifecycle.ViewModel
+
+/**
+ * @author chenweiming
+ * @version 1.0
+ * @mail cwm930215@gmail.com
+ * @since 2020/7/12 9:57 PM
+ **/
+class MyViewModel : ViewModel() {
+    var number = 0;
+}
