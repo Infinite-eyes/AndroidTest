d2384bd4ae739eb8dd4d199af9a81590610c6012
Infinite-eyes <cwm930215@gmail.com>
2020-08-02 22:43:20 +0800

    1.add motion editor

---
+++ app/src/main/java/com/demo/motioneditor/MotionEditorActivity.kt
@@ -0,0 +1,24 @@
+package com.demo.motioneditor
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.demo.androidtest.R
+
+/**
+ * @author chenweiming
+ * @version 1.0
+ * @mail cwm930215@gmail.com
+ * @since 2020/8/2 10:23 PM
+ **/
+class MotionEditorActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.motioneditor_activity)
+
+
+
+    }
+
+
+}
