b6e17c4ac354dbdf6cb35c6d48283e53059710c2
Infinite-eyes <cwm930215@gmail.com>
2020-08-03 23:43:36 +0800

    1.add coordinatorlayout test

--- app/src/main/java/com/demo/App.kt
+++ app/src/main/java/com/demo/App.kt
@@ -1,15 +1,6 @@
 package com.demo
 
 import android.app.Application
-import android.content.Context
-import com.scwang.smart.refresh.footer.ClassicsFooter
-import com.scwang.smart.refresh.header.ClassicsHeader
-import com.scwang.smart.refresh.layout.SmartRefreshLayout
-import com.scwang.smart.refresh.layout.api.RefreshFooter
-import com.scwang.smart.refresh.layout.api.RefreshHeader
-import com.scwang.smart.refresh.layout.api.RefreshLayout
-import com.scwang.smart.refresh.layout.listener.DefaultRefreshFooterCreator
-import com.scwang.smart.refresh.layout.listener.DefaultRefreshHeaderCreator
 
 
 /**
@@ -19,6 +10,5 @@ import com.scwang.smart.refresh.layout.listener.DefaultRefreshHeaderCreator
  * @since 2020/7/23 2:08 PM
  **/
 public class App : Application() {
-
 
 }
---
+++ app/src/main/java/com/demo/materialdesign/coordinatorlayout/CoordinatorLayoutActivity.kt
@@ -0,0 +1,21 @@
+package com.demo.materialdesign.coordinatorlayout
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.demo.androidtest.R
+
+/**
+ * @author chenweiming
+ * @version 1.0
+ * @mail cwm930215@gmail.com
+ * @since 2020/8/3 12:32 AM
+ **/
+class CoordinatorLayoutActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.coordinatorlayout_activity)
+
+    }
+
+}
