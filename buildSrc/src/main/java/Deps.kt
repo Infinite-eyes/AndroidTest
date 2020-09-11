bd3e446ab4b0bad001592a8282e4c8ebebcd9ec0
Infinite-eyes <cwm930215@gmail.com>
2020-09-11 16:21:38 +0800

    1.add buildScr

---
+++ buildSrc/src/main/java/Deps.kt
@@ -0,0 +1,19 @@
+/**
+ * @author chenweiming
+ * @version 1.0
+ * @mail cwm930215@gmail.com
+ * @since 2020/9/11 4:14 PM
+ **/
+
+object Versions {
+
+    const val appcompat = "1.1.0"
+
+}
+
+
+object Deps {
+
+    const val appcompat = "androidx.appcompat:appcompat:${Versions.appcompat}"
+
+}
